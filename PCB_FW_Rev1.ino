30a8332df5d43af4e8a7f61eabfc2cea40c8057b
vpagano12 <98890398+vpagano12@users.noreply.github.com>
2024-04-18 15:43:17 -0400

    I2C Timing Implemented

--- PCB_FW_Rev1.ino
+++ PCB_FW_Rev1.ino
@@ -4,16 +4,19 @@
 #define D1 12 //GPIO_NUM_27
 #define D2 13 //GPIO_NUM_14 
 #define D3 14 //GPIO_NUM_12
-#define doppler_in GPIO_NUM_2 // 24
+//#define doppler_in GPIO_NUM_2 // 24
+#define doppler_in GPIO_NUM_25 // signal in pin for breadboard testing
 #define I2C_SDA 33 //GPIO_NUM_21
 #define I2C_SCL 36 //GPIO_NUM_22
 
 /* other useful definitions */
 //#define doppler_interval 10000
-#define I2C_interval 10000 /* time for which ESP can sleep, 
-                           assuming no other interruptions, before waking to send I2C data */
+#define I2C_interval 8000000 /* time for which ESP can sleep, (uS; 1S = 1000000uS)
+                             assuming no other interruptions, before waking to send I2C data */
 
 RTC_DATA_ATTR int bootCount = 0; // track # of times ESP boots for initialization purposes
+RTC_DATA_ATTR int i2cTime_Start = 0;
+RTC_DATA_ATTR int i2cTime_Elapsed = 0;
 
 /* this method stores the reason the ESP wakes up; used to determine how to proceed further in setup() function */
 int wakeup_reason() {
@@ -32,14 +35,38 @@ int wakeup_reason() {
 }
 
 // interrupt that is entered when motion is detected
-void IRAM_ATTR_ISR() {
+void IRAM_ATTR motionDetect() {
+
+//  int t_ISR_start = millis();
+
+//  detachInterrupt(doppler_in); // now pin is ignored
+
+  Serial.begin(115200);
+//  delay(1000);
+
+  Serial.println("Woken up by motion ISR");
+
+  
   
 }
 
+// deep sleep stub; runs immediately after coming out of deep sleep
+
+//void esp_wake_deep_sleep() {
+//  Serial.begin(115200);
+//  delay(1000);
+//  esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_EXT0);
+//
+//  Serial.println("Deep sleep stub");
+//}
+
+// function to handle motion detection
+
 void setup() {
 
-  // initialization routine, first boot
-  if (bootCount == 0) {
+  if (bootCount == 0) {  // initialization routine, runs on first boot
+    Serial.begin(115200);
+    delay(1000);
     pinMode(D1, INPUT);
     pinMode(D2, INPUT);
     pinMode(D3, INPUT);
@@ -47,8 +74,9 @@ void setup() {
     pinMode(I2C_SDA, INPUT);
     pinMode(I2C_SCL, INPUT);
     esp_sleep_enable_ext0_wakeup(doppler_in, 1);
-    attachInterrupt(doppler_in, IRAM_ATTR_ISR, ONHIGH); //attach interrupt to enable "ignore" behavior for motion detection
+    attachInterrupt(doppler_in, motionDetect, ONHIGH); //attach interrupt to enable "ignore" behavior for motion detection
     esp_sleep_enable_timer_wakeup(I2C_interval);
+    i2cTime_Start = millis();
     bootCount++;
     Serial.println("Boot Count: " + String(bootCount));
     Serial.println("Initialization Complete");
@@ -57,30 +85,53 @@ void setup() {
     esp_deep_sleep_start(); //begin deep sleep
   }
 
-  else if (bootCount > 0) {
+  else if (bootCount > 0) { // at some point, check this value and roll it over to 1 so it doesn't overflow
     Serial.begin(115200);
     delay(1000);
     int wake = wakeup_reason();
     if (wake == 1) {
       Serial.println("Woken up by motion");
-      esp_sleep_enable_ext0_wakeup(doppler_in, 1); /* re-initialize doppler pin for wakeup mode */
-      // server transmission can go here 
+      i2cTime_Elapsed = (millis() + i2cTime_Elapsed) * 1000; // conversion from milliseconds to microseconds for sleep instructions
+      Serial.println("Elapsed Time After Motion: " + String(i2cTime_Elapsed));
+      Serial.println("I2C Timer: " + String(I2C_interval-i2cTime_Elapsed));
+      if ((I2C_interval-i2cTime_Elapsed) < 0) { // if the result is negative, just pass in 0 for the timer duration
+         esp_sleep_enable_timer_wakeup(0);
+      }
+      else {
+         esp_sleep_enable_timer_wakeup(I2C_interval-i2cTime_Elapsed);
+      }
+      esp_sleep_enable_ext0_wakeup(doppler_in, 1);
+      // server transmission can go here
+      bootCount++;
+      Serial.println("Boot Count: " + String(bootCount));
+      Serial.println("Sleeping...\n");
+      Serial.println("");
+      Serial.flush(); //"Waits for the transmission of outgoing serial data to complete."
+      esp_deep_sleep_start(); //begin deep sleep   
+
     }
     else if (wake == 2) {
       Serial.println("Woken up by timer");
-      esp_sleep_enable_timer_wakeup(I2C_interval); /* double check that if woken up by timer, doppler sleep enable on ext0 persists
-                                                   after boot, and vice-versa */
-      // server transmission can go here                                            
+      i2cTime_Elapsed = 0; // timer has fired; reset i2c times       
+      esp_sleep_enable_timer_wakeup(I2C_interval); // re-enable timer as a wakeup source                                   
+      esp_sleep_enable_ext0_wakeup(doppler_in, 1); // re-enable doppler as a wakeup source  
+      // server transmission can go here
+      bootCount++;
+      Serial.println("Boot Count: " + String(bootCount));
+      Serial.println("Sleeping...\n");
+      Serial.println("");
+      Serial.flush(); //"Waits for the transmission of outgoing serial data to complete."
+      i2cTime_Start = millis();   
+      esp_deep_sleep_start(); //begin deep sleep                                         
     }
     else {
       Serial.println("Woken up for unknown reason");
+      Serial.println("Boot Count: " + String(bootCount));
+      Serial.println("Sleeping...\n");
+      Serial.flush(); //"Waits for the transmission of outgoing serial data to complete."
+      esp_deep_sleep_start(); //begin deep sleep
     }
-    bootCount++;
-    Serial.println("Boot Count: " + String(bootCount));
-    Serial.println("Sleeping...");
-    Serial.println("");
-    Serial.flush(); //"Waits for the transmission of outgoing serial data to complete."
-    esp_deep_sleep_start(); //begin deep sleep
+
   }
   
 }
